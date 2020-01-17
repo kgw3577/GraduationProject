912340197e1c92c13521b8e61c2637ddaf60f5fb
lynnfield1 <38973297+lynnfield1@users.noreply.github.com>
2020-01-17 20:33:58 +0900

    프로젝트 다시 생성

---
+++ app/src/main/java/com/Project/Closet/activity_home.java
@@ -0,0 +1,15 @@
+package com.Project.Closet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_home);
+    }
+
+}
---
+++ app/src/main/java/com/Project/Closet/activity_login.java
@@ -0,0 +1,136 @@
+package com.Project.Closet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.AsyncTask;
+import java.io.*;
+import java.net.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_login extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    int count = 0;
+
+    EditText userId, userPwd;
+    Button loginBtn, joinBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        BtnOnClickListener onClickListener = new BtnOnClickListener() ;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.layout_login);
+        Button btnLogin = findViewById(R.id.bt_login);
+        btnLogin.setOnClickListener(onClickListener);
+
+        userId = (EditText) findViewById(R.id.et_Email);
+        userPwd = (EditText) findViewById(R.id.et_Password);
+        loginBtn = (Button) findViewById(R.id.bt_login);
+        joinBtn = (Button) findViewById(R.id.bt_join);
+    }
+
+    class CustomTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("http://192.168.43.23:8080/Android/test.jsp");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "id="+strings[0]+"&pwd="+strings[1]+"&type="+strings[2];
+                osw.write(sendMsg);
+                osw.flush();
+                if(conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+
+                } else {
+                    Log.i("통신 결과", conn.getResponseCode()+"에러");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return receiveMsg;
+        }
+    }
+
+
+
+    class BtnOnClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                /*
+                    case R.id.loginBtn :
+                    Intent intent = new Intent(getApplicationContext(), activity_home.class);
+                    startActivity(intent);
+                    break;
+                 */
+                case R.id.bt_login : // 로그인 버튼 눌렀을 경우
+                    String loginid = userId.getText().toString();
+                    String loginpwd = userPwd.getText().toString();
+                    try {
+                        String result  = new CustomTask().execute(loginid,loginpwd,"login").get();
+                        if(result.equals("true")) {
+                            Toast.makeText(activity_login.this,"로그인",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(activity_login.this, activity_login.class);
+                            startActivity(intent);
+                            finish();
+                        } else if(result.equals("false")) {
+                            Toast.makeText(activity_login.this,"아이디 또는 비밀번호가 틀렸음",Toast.LENGTH_SHORT).show();
+                            userId.setText("");
+                            userPwd.setText("");
+                        } else if(result.equals("noId")) {
+                            Toast.makeText(activity_login.this, "존재하지 않는 아이디", Toast.LENGTH_SHORT).show();
+                            userId.setText("");
+                            userPwd.setText("");
+                        } else {
+                            Toast.makeText(activity_login.this, result, Toast.LENGTH_SHORT).show();
+                            //Log.i("리턴 값", result);
+                        }
+                    }catch (Exception e) {}
+                    break;
+                case R.id.bt_join : // 회원가입
+                    String joinid = userId.getText().toString();
+                    String joinpwd = userPwd.getText().toString();
+                    try {
+                        String result  = new CustomTask().execute(joinid,joinpwd,"join").get();
+                        if(result.equals("id")) {
+                            Toast.makeText(activity_login.this,"이미 존재하는 아이디입니다.",Toast.LENGTH_SHORT).show();
+                            userId.setText("");
+                            userPwd.setText("");
+                        } else if(result.equals("ok")) {
+                            userId.setText("");
+                            userPwd.setText("");
+                            Toast.makeText(activity_login.this,"회원가입을 축하합니다.",Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (Exception e) {}
+                    break;
+            }
+        }
+    }
+}
