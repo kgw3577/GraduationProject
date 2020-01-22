ef11f69496433bcbf9eebeb744f5508cca200163
JJW <wjdwldn0943@gmail.com>
2020-01-22 09:50:31 +0900

    회원가입 로직 추가

--- app/src/main/java/com/Project/Closet/activity_signup.java
+++ app/src/main/java/com/Project/Closet/activity_signup.java
@@ -2,13 +2,108 @@ package com.Project.Closet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class activity_signup extends AppCompatActivity {
+
+    EditText userId, userPwd;
+    Button joinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        BtnOnClickListener onClickListener = new BtnOnClickListener() ;
         setContentView(R.layout.layout_signup);
+        Button btnSignup = findViewById(R.id.bt_join);
+        btnSignup.setOnClickListener(onClickListener);
+
+        userId = (EditText) findViewById(R.id.et_Email);
+        userPwd = (EditText) findViewById(R.id.et_Password);
+        joinBtn = (Button) findViewById(R.id.bt_join);
+    }
+
+    class SignupTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("http://192.168.0.208:8080/Android/Join.jsp");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "id=" + strings[0] + "&pwd=" + strings[1] + "&type=" + strings[2];
+                osw.write(sendMsg);
+                osw.flush();
+                if (conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+
+                } else {
+                    Log.i("통신 결과", conn.getResponseCode() + "에러");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return receiveMsg;
+        }
+    }
+
+
+    class BtnOnClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                /*
+                    case R.id.loginBtn :
+                    Intent intent = new Intent(getApplicationContext(), activity_home.class);
+                    startActivity(intent);
+                    break;
+                 */
+                case R.id.bt_join: // 회원가입 버튼 눌렀을 경우
+                    String joinid = userId.getText().toString();
+                    String joinpwd = userPwd.getText().toString();
+                    try {
+                        String result  = new activity_signup.SignupTask().execute(joinid, joinpwd, "join").get();
+                        if(result.equals("id")) {
+                            Toast.makeText(activity_signup.this,"이미 존재하는 아이디입니다.",Toast.LENGTH_SHORT).show();
+                            userId.setText("");
+                            userPwd.setText("");
+                        } else if(result.equals("ok")) {
+                            userId.setText("");
+                            userPwd.setText("");
+                            Toast.makeText(activity_signup.this,"회원가입을 축하합니다.",Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (Exception e) {}
+                    break;
+
+                // 뒤로 돌아가는 버튼 추가하기
+            }
+        }
     }
 }
