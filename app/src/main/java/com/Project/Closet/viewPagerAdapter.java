210bd20b56be83319ddc7943169094add5f9b87a
lynnfield1 <38973297+lynnfield1@users.noreply.github.com>
2020-01-27 18:55:22 +0900

    메인폼 업데이트
    1. viewpager업데이트
    2. 메뉴 업데이트
    3. 탭레이아웃추가
    
    추후 레이아웃 변경예정

---
+++ app/src/main/java/com/Project/Closet/viewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.Project.Closet;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class viewPagerAdapter extends PagerAdapter {
+    private Context mContext;
+    private ArrayList<Integer> imageList;
+
+    public viewPagerAdapter(Context context, ArrayList<Integer> imageList)
+    {
+        this.mContext = context;
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.layout_pagerview, null);
+
+        ImageView imageView = view.findViewById(R.id.pager_image);
+        imageView.setImageResource(imageList.get(position));
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return imageList.size();
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return (view == (View)o);
+    }
+}
