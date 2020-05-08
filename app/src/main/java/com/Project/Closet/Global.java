694823ce21b0a5f84e3ca7100e49046ea164bef0
chocoratepie <chocoratepie@gmail.com>
2020-05-09 00:15:51 +0900

    IP 주소 변경 및 MySQL 관련 수정사항 반영

--- app/src/main/java/com/Project/Closet/Global.java
+++ app/src/main/java/com/Project/Closet/Global.java
@@ -2,7 +2,7 @@ package com.Project.Closet;
 
 public class Global {
     //public static final String baseURL = "http://192.168.0.3:8080/";     //로컬 주소
-    public static final String baseURL = "http://54.180.99.123:8080/"; //AWS 주소
+    public static final String baseURL = "http://13.124.211.71:8080/"; //AWS 주소
 
     private static Global instance = null;
 
